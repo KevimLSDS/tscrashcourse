1cac53239406289c636edf731130573e98972c83
KevimLSDS <kevim.lhouis@outlook.com>
2022-08-15 21:01:09 -0300

    Loops and Arrays

---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportsOne: string[] = ['Golf', 'Cricket', 'Tennis', 'Swimming'];
+
+for (let tempSport of sportsOne) {
+    if (tempSport == 'Cricket') {
+        console.log(`${tempSport} is my favorite!`);
+    } else {
+        console.log(tempSport);
+    }
+}
